ff74bd35f731edb4756af6e37def92264bfae704
halubko <halubko@MacBook-Air-halubko.local>
2025-12-17 23:42:43 +0300

    init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,30 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettier from "eslint-plugin-prettier";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+   ...nextVitals,
+   ...nextTs,
+   eslintConfigPrettier,
+   {
+      plugins: {
+         prettier: eslintPluginPrettier,
+      },
+      rules: {
+         "prettier/prettier": "warn",
+         "no-console": "warn",
+      },
+   },
+   // Override default ignores of eslint-config-next.
+   globalIgnores([
+      // Default ignores of eslint-config-next:
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+   ]),
 ]);
 
 export default eslintConfig;
