dab87c7d17d08965756dc759bd225eb93c387b50
halubko <misha.golubko@gmail.com>
2025-12-24 20:22:11 +0300

    fix: tailwind css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Toaster } from "@/shared/components/ui/sonner";
 import type { ReactNode } from "react";
+import "@/app/styles/globals.css";
 
 export const metadata: Metadata = {
    title: "Shop",
