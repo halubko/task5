f64f9b371d0afc997cbafded97767822acf359e5
unknown <dmshastel@gmail.com>
2025-12-25 21:06:46 +0300

    The sigIn is created, tailwind is pluged in

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Toaster } from "@/shared/components/ui/sonner";
 import type { ReactNode } from "react";
+import "./styles/globals.css";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
    return (
