3a0fe9cd345e7091db918d8d180efae84da9da29
unknown <dmshastel@gmail.com>
2025-12-27 21:24:21 +0300

    The shoppingCart is done

---
+++ src/app/shoppingCart/page.tsx
@@ -0,0 +1,9 @@
+import ShoppingCart from "@/shared/components/ui/ShoppingCart";
+
+export default function ShoppingCartPage() {
+   return (
+      <div>
+         <ShoppingCart />
+      </div>
+   );
+}
