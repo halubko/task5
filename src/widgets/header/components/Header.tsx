8c90b7ba05b34056e573a85d7481a250103be5f1
halubko <misha.golubko@gmail.com>
2025-12-30 00:26:10 +0300

    fix: adaptive

--- src/widgets/header/components/Header.tsx
+++ src/widgets/header/components/Header.tsx
@@ -4,6 +4,7 @@ import { ThemeToggleButton } from "@/features/toggle-theme/components/ThemeToogl
 import { SidebarTrigger } from "@/shared/components/ui/sidebar";
 import ProfileDropdown from "@/widgets/header/components/ui/ProfileDropdown";
 import Link from "next/link";
+import BackButton from "@/features/go-back/components/BackButton";
 
 interface HeaderProps {
    isMainPage?: boolean;
@@ -11,12 +12,14 @@ interface HeaderProps {
 
 const Header = ({ isMainPage = false }: HeaderProps) => {
    return (
-      <header className="grid grid-cols-3 items-center p-2">
-         <div className="flex gap-2">{isMainPage && <SidebarTrigger />}</div>
-         <Link href="/">
-            <h1 className="justify-self-center">KakaShop</h1>
-         </Link>
-         <div className="flex gap-2 justify-end">
+      <header className="grid grid-cols-2 items-center p-2 sticky top-0 z-10 bg-black">
+         <div className="flex items-center gap-2">
+            {isMainPage ? <SidebarTrigger /> : <BackButton />}
+            <Link href="/" className="flex items-center">
+               <h1 className="font-semibold text-md md:text-xl">KakaShop</h1>
+            </Link>
+         </div>
+         <div className="flex gap-2 justify-end items-center">
             <ThemeToggleButton />
             <Link href="/cart">
                <Button>
