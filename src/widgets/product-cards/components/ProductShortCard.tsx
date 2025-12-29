ea7de9f03b5aa06b7ac98f96c71bfdacd88faa75
halubko <misha.golubko@gmail.com>
2025-12-30 01:59:27 +0300

    fix: adaptive

--- src/widgets/product-cards/components/ProductShortCard.tsx
+++ src/widgets/product-cards/components/ProductShortCard.tsx
@@ -2,7 +2,7 @@ import {
    Card,
    CardAction,
    CardContent,
-   CardDescription,
+   // CardDescription,
    CardFooter,
    CardHeader,
    CardTitle,
@@ -13,22 +13,28 @@ import Link from "next/link";
 import AddToCartButton from "@/features/add-to-cart/components/AddToCartButton";
 import ProductImages from "@/entities/product/components/ProductImages";
 
-const ProductShortCard = ({ title, price, description, images, rating, id }: ProductInterface) => {
+const ProductShortCard = ({
+   title,
+   price,
+   /*description,*/ images,
+   rating,
+   id,
+}: ProductInterface) => {
    return (
       <Card>
          <CardHeader>
             <Link href={`/${id}`}>
-               <CardTitle>{title}</CardTitle>
+               <CardTitle className="text-ellipsis overflow-hidden line-clamp-1">{title}</CardTitle>
             </Link>
          </CardHeader>
          <CardContent>
             <ProductImages images={images} id={id} title={title} />
             <Link href={`/${id}`}>
-               <CardDescription className="text-ellipsis overflow-hidden line-clamp-3">
-                  {description}
-               </CardDescription>
+               {/*<CardDescription className="text-ellipsis overflow-hidden line-clamp-3">*/}
+               {/*   {description}*/}
+               {/*</CardDescription>*/}
                <div className="w-full flex justify-between">
-                  <span className="mt-2">Price: {price}$</span>
+                  <div className="mt-2">{price}$</div>
                   <span className="mt-2 flex gap-1">{<ProductRating rating={rating} />}</span>
                </div>
             </Link>
