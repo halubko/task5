07b552ae675dd228632a7f9eadb3cf5a7703cae7
unknown <dmshastel@gmail.com>
2026-01-03 14:15:40 +0300

    The signInStore interfaces are added

---
+++ src/shared/stores/interfaces.ts
@@ -0,0 +1,16 @@
+export interface SignInData {
+   username: string;
+   password: string;
+}
+
+export interface SignInDataResponse {
+   id: number;
+   username: string;
+   email: string;
+   firstName: string;
+   lastName: string;
+   gender: string;
+   image: string;
+   accessToken: string;
+   refreshToken: string;
+}
