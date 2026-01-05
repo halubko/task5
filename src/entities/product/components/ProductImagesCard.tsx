141ffbeb5cd8fcf1f07389f1e51916e6b7d425c7
halubko <misha.golubko@gmail.com>
2026-01-05 19:52:27 +0300

    fix: decompose images carousel

--- src/entities/product/components/ProductImagesCard.tsx
+++ src/entities/product/components/ProductImagesCard.tsx
@@ -1,14 +1,7 @@
 import React from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/shared/components/ui/card";
 import type { ProductInterface } from "@/widgets/product-cards/interfaces/product.interfaces";
-import {
-   Carousel,
-   CarouselContent,
-   CarouselItem,
-   CarouselNext,
-   CarouselPrevious,
-} from "@/shared/components/ui/carousel";
-import Image from "next/image";
+import ProductImagesCardCarousel from "@/entities/product/components/ui/ProductImagesCardCarousel";
 
 const ProductImagesCard = ({ title, images }: ProductInterface) => {
    return (
@@ -17,30 +10,7 @@ const ProductImagesCard = ({ title, images }: ProductInterface) => {
             <CardTitle>{title}</CardTitle>
          </CardHeader>
          <CardContent>
-            <Carousel className="w-full">
-               <CarouselContent>
-                  {images.map((image, index) => (
-                     <CarouselItem key={image} className="flex justify-center items-center">
-                        <div className="relative w-full h-125  md:h-150">
-                           <Image
-                              src={image}
-                              alt={title + index}
-                              fill
-                              sizes="contain"
-                              loading="eager"
-                              className="object-contain mx-auto max-w-xs md:max-w-xl h-fit"
-                           />
-                        </div>
-                     </CarouselItem>
-                  ))}
-               </CarouselContent>
-               {images.length > 1 && (
-                  <>
-                     <CarouselPrevious className="left-2" />
-                     <CarouselNext className="right-2" />
-                  </>
-               )}
-            </Carousel>
+            <ProductImagesCardCarousel title={title} images={images} />
          </CardContent>
       </Card>
    );
---
+++ src/entities/product/components/ui/ProductImagesCardCarousel.tsx
@@ -0,0 +1,39 @@
+import {
+   Carousel,
+   CarouselContent,
+   CarouselItem,
+   CarouselNext,
+   CarouselPrevious,
+} from "@/shared/components/ui/carousel";
+import Image from "next/image";
+
+const ProductImagesCardCarousel = ({ title, images }: { title: string; images: string[] }) => {
+   return (
+      <Carousel className="w-full">
+         <CarouselContent>
+            {images.map((image, index) => (
+               <CarouselItem key={image} className="flex justify-center items-center">
+                  <div className="relative w-full h-125  md:h-150">
+                     <Image
+                        src={image}
+                        alt={title + index}
+                        fill
+                        sizes="contain"
+                        loading="eager"
+                        className="object-contain mx-auto max-w-xs md:max-w-xl h-fit"
+                     />
+                  </div>
+               </CarouselItem>
+            ))}
+         </CarouselContent>
+         {images.length > 1 && (
+            <>
+               <CarouselPrevious className="left-2" />
+               <CarouselNext className="right-2" />
+            </>
+         )}
+      </Carousel>
+   );
+};
+
+export default ProductImagesCardCarousel;
