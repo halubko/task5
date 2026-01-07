5499cd707294cb670f1b64453925c1d97d3999f9
halubko <misha.golubko@gmail.com>
2026-01-07 18:19:26 +0300

    fix: added max width

--- src/widgets/productsList/ui/ProductsList.tsx
+++ src/widgets/productsList/ui/ProductsList.tsx
@@ -6,7 +6,7 @@ const ProductsList = async ({ searchParams }: ProductsListInterface) => {
    const { products } = await getProducts(searchParams);
 
    return (
-      <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1 row-auto p-2 gap-2">
+      <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1 row-auto p-2 gap-2 max-w-7xl mx-auto">
          {products.map((product) => (
             <ProductShortCard key={product.id} {...product} />
          ))}
