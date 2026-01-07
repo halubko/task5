01e5a74c2f8430389c9f84bedb7a20a7a3193610
halubko <misha.golubko@gmail.com>
2026-01-08 00:47:42 +0300

    fix: added try catch

--- src/widgets/productsFilter/api/api.ts
+++ src/widgets/productsFilter/api/api.ts
@@ -1,11 +1,18 @@
-import axios, { type AxiosResponse } from "axios";
+import axios, { type AxiosResponse, isAxiosError } from "axios";
 import { BASE_URL } from "@/shared/constants/constants";
+import { toast } from "sonner";
 
 const axiosInstance = axios.create({
    baseURL: BASE_URL + "products",
 });
 
 export const getProductCategoryList = async () => {
-   const response: AxiosResponse<string[]> = await axiosInstance.get("/category-list");
-   return response.data;
+   try {
+      const response: AxiosResponse<string[]> = await axiosInstance.get("/category-list");
+      return response.data;
+   } catch (error) {
+      if (isAxiosError(error)) {
+         toast.error("Loading categories error", { description: error.message });
+      }
+   }
 };
