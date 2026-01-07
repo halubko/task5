33d9fe8e4fa4fcfb724d7178af3e8919ac4f6b8f
halubko <misha.golubko@gmail.com>
2026-01-08 00:51:21 +0300

    fix: deleting filter from header

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -3,14 +3,13 @@ import { ShoppingCartIcon } from "lucide-react";
 import { ThemeToggleButton } from "@/features/toggleTheme/ui/ThemeToogleButton";
 import Link from "next/link";
 import BackButton from "@/features/goBack/ui/BackButton";
-import { ProductsFilter } from "@/widgets/productsFilter/ui/ProductsFilter";
 import AuthButton from "@/widgets/header/ui/AuthButton";
 
-const Header = ({ isMainPage = false }: { isMainPage?: boolean }) => {
+const Header = () => {
    return (
       <header className="grid grid-cols-2 items-center p-2 sticky top-0 z-10 backdrop-blur">
          <div className="flex items-center gap-2">
-            {isMainPage ? <ProductsFilter /> : <BackButton />}
+            <BackButton />
             <Link href="/" className="flex items-center">
                <h1 className="font-semibold text-md md:text-xl">KakaShop</h1>
             </Link>
