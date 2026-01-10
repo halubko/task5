8f9e236f342f75929b500eb8809040393ae2938c
halubko <misha.golubko@gmail.com>
2026-01-10 21:08:06 +0300

    fix: minor fix

--- src/entities/product/tests/formatters.test.ts
+++ src/entities/product/tests/formatters.test.ts
@@ -5,8 +5,8 @@ describe("formatters", () => {
    test("should return date (numeric:long:numeric)", () => {
       const mockDate = "2024-05-23T08:56:21.618Z";
 
-      const resalt = formatDate(mockDate);
+      const result = formatDate(mockDate);
 
-      expect(resalt).toBe("May 23, 2024");
+      expect(result).toBe("May 23, 2024");
    });
 });
